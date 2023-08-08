32d0896642a10ff307c0d13ad3d505677862b760
Tetiana Siestova <siestova.t@gmail.com>
2023-08-08 14:43:57 +0300

    burger-menu

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import "./style.scss";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const btnMenu = document.querySelector(".burger-menu");
+  const header = document.querySelector(".header");
+
+  btnMenu.addEventListener("click", () => {
+    header.toggleAttribute("data-active");
+  });
+});
